eec9fd7c0f9ff6c1238f5387f6442ba1f6d5cfff
Antoni Batchelli <tbatchelli@acm.org>
2014-05-21 09:18:35 -0700

    Update dep to hyde 1.1-SNAPSHOT

--- hyde-pallet-core/project.clj
+++ hyde-pallet-core/project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-modules "0.3.1"]]
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.palletops/hyde "0.1.0"]])
+                 [com.palletops/hyde "0.1.1-SNAPSHOT"]])
