cc5bea0090f4823f65e020979de8b9e15ef97886
Hugo Duncan <hugo@hugoduncan.org>
2014-05-20 21:43:57 -0400

    Add lein-hyde

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject com.palletops/hyde-pallet "0.1.0-SNAPSHOT"
-  :description "Site generation for clojure projects"
+  :description "Parent for hyde-pallet"
   :url "https://github.com/paleltops/hyde-pallet"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[com.palletops/hyde-pallet "0.1.0-SNAPSHOT"]]
   :plugins [[lein-modules "0.3.1"]]
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.palletops/hyde "0.1.0"]])
+  :aliases {"install" ["modules" "install"]
+            "deploy" ["modules" "deploy"]
+            "clean" ["modules" "clean"]})
